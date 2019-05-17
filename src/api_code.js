ca46e9082452b77917cce4afa9852d38cfddc5ab
xinconan <xinconan2@sina.com>
2019-05-17 09:35:22 +0800

    feat: add qrcode api

--- src/api_code.js
+++ src/api_code.js
@@ -18,5 +18,53 @@ exports.createQRCode = function (accessToken, path, width = 430) {
     path,
     width,
   }
-  return axios.post('https://api.weixin.qq.com/sns/jscode2session', params)
+  return axios.post('https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode', params)
+}
+
+/**
+ * 获取小程序码，适用于需要的码数量较少的业务场景。
+ * https://developers.weixin.qq.com/miniprogram/dev/api-backend/wxacode.get.html
+ * @param accessToken
+ * @param path
+ * @param width
+ * @param autoColor
+ * @param lineColor
+ * @param isHyaline
+ * @returns {AxiosPromise<any>}
+ */
+exports.getQRCode = function (accessToken, path, width = 430, autoColor=false, lineColor={"r":0,"g":0,"b":0}, isHyaline=false) {
+  const params = {
+    access_token: accessToken,
+    path,
+    width,
+    auto_color: autoColor,
+    line_color: lineColor,
+    is_hyaline: isHyaline
+  }
+  return axios.post('https://api.weixin.qq.com/wxa/getwxacode', params)
+}
+
+/**
+ * 获取小程序码，适用于需要的码数量极多的业务场景。
+ * https://developers.weixin.qq.com/miniprogram/dev/api-backend/wxacode.getUnlimited.html
+ * @param accessToken
+ * @param scence
+ * @param page
+ * @param width
+ * @param autoColor
+ * @param lineColor
+ * @param isHyaline
+ * @returns {AxiosPromise<any>}
+ */
+exports.getUnlimited = function (accessToken, scence, page, width = 430, autoColor=false, lineColor={"r":0,"g":0,"b":0}, isHyaline=false) {
+  const params = {
+    access_token: accessToken,
+    scene,
+    path,
+    width,
+    auto_color: autoColor,
+    line_color: lineColor,
+    is_hyaline: isHyaline
+  }
+  return axios.post('https://api.weixin.qq.com/wxa/getwxacodeunlimit', params)
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,8 @@ declare class WxApi {
   getAccessToken(): object
   code2Session(code: string): object
   createQRCode(accessToken: string, path: string, width?: number): object
+  getQRCode(accessToken: string, path: string, width?: number, autoColor?: boolean, lineColor?: object, isHyaline?: boolean): object
+  getUnlimited(accessToken: string, scene: string, page?: string, width?: number, autoColor?: boolean, lineColor?: object, isHyaline?: boolean): object
 }
 
 export default WxApi
